109e2fce7f78a5bc046a9c9b453758d4163df68e
muhammad luqman <144345295+MuhammadLuqmanSi@users.noreply.github.com>
2024-11-19 14:40:17 +0800

    Add files via upload

---
+++ src/Model/Account.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Luqman
+ */
+abstract class Account {
+    final String email;
+    final private String password;
+    
+    public Account(String email, String password){
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    
+}
+
+ 
